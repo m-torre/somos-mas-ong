688f9abc7dbf8f0ab1ace03b58dce7b9db83b235
Maximiliano Torre <m.torre164@gmail.com>
2022-09-28 17:47:08 -0300

    Add Umzug to perform migrations automatically

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,5 +1,7 @@
 const Sequelize = require("sequelize");
 const { DATABASE_URI } = require("./config");
+const { Umzug, SequelizeStorage } = require("umzug");
+const path = require("path");
 const logger = require("./logger");
 
 const sequelize = new Sequelize(DATABASE_URI, {
@@ -11,9 +13,37 @@ const sequelize = new Sequelize(DATABASE_URI, {
   },
 });
 
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: {
+      glob: [
+        "{migrations/*.js,seeders/*.js}",
+        { cwd: path.resolve(__dirname, "../") },
+      ],
+      resolve: ({ name, path, context }) => {
+        const migration = require(path);
+        return {
+          name,
+          up: async () => migration.up(context, Sequelize),
+          down: async () => migration.down(context, Sequelize),
+        };
+      },
+    },
+    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+    context: sequelize.getQueryInterface(),
+    logger: console,
+  });
+
+  const migrations = await migrator.up();
+  logger.info("Migrations up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
+    await runMigrations();
     logger.info("Connection to the database established successfully.");
   } catch (error) {
     logger.error("Unable to connect to the database:", error);
